e60e3f619479ef83ac6f6175512e08a31a1d85e4
FiloBolis <125869579+FiloBolis@users.noreply.github.com>
2025-01-03 23:27:16 +0100

    aggiunta CSS login

--- login/login.php
+++ login/login.php
@@ -13,27 +13,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Login</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="../css/login.css">
 </head>
-<body>
-    <?php 
-        if (isset($_GET["messaggio"])) {
-            echo $_GET["messaggio"];
-        }
-    ?>
-    
-    <form action="login_manager.php" method="get">
-        <label for="usernameTxt">Username: </label>
-        <input type="text" id="usernameTxt" name="username"><br>
+<body>  
+    <div class="wrapper">
+        <form action="login_manager.php" method="get">
+            <h1>Login</h1>
+            <div class="input-box">
+                <input type="text" name="username" placeholder="Username" required>
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="input-box">
+                <input type="password" name="password" placeholder="Password" required>
+                <i class="fa-solid fa-lock"></i>
+            </div>
 
-        <label for="passwordTxt">Password: </label>
-        <input type="password" id="passwordTxt" name="password"><br>
+            <button type="submit" class="btn">Login</button>
 
-        <button>Login</button>
-        <input type="reset" value="Reset">
-    </form>
-
-    <a href="register.php">Registrati</a>
+            <div class="register-link">
+                <p>Don't have an account? <a href="register.php">Register</a></p>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
